1e23de92f02121a2b1d08374b854390467957765
benjitaa898 <benja.frojas21@gmail.com>
2024-10-21 16:28:02 -0300

    se sube archivo de python

---
+++ benja.py
@@ -0,0 +1,82 @@
+#condicional si
+#ingrese 5 usuarios 
+#debe ingresar nombre y color de equipo o alianza
+cont_rojo = 0
+cont_azul = 0
+cont_amarillo = 0 
+#USUARIO 1
+
+user1=input("ingrese nombre de usuario:")
+color1=input("ingrese color de su alianza:")
+
+if color1 == "rojo":
+    cont_rojo = cont_rojo+1
+
+if color1 == "azul":
+    cont_azul = cont_azul+1
+
+if color1 == "amarillo":
+    cont_amarillo = cont_amarillo+1
+
+#USUARIO 2
+
+user2=input("ingrese nombre de usuario:")
+color2=input("ingrese color de su alianza:")
+
+if color2 == "rojo":
+    cont_rojo = cont_rojo+1
+
+if color2 == "azul":
+    cont_azul = cont_azul+1
+
+if color2 == "amarillo":
+    cont_amarillo = cont_amarillo+1
+
+#USUARIO 3
+
+user3=input("ingrese nombre de usuario:")
+color3=input("ingrese color de su alianza:")
+
+if color3 == "rojo":
+    cont_rojo = cont_rojo+1
+
+if color3 == "azul":
+    cont_azul = cont_azul+1
+
+if color3 == "amarillo":
+    cont_amarillo = cont_amarillo+1
+
+#USUARIO 4
+
+user4=input("ingrese nombre de usuario:")
+color4=input("ingrese color de su alianza:")
+
+if color4 == "rojo":
+    cont_rojo = cont_rojo+1
+
+if color4 == "azul":
+    cont_azul = cont_azul+1
+
+if color4 == "amarillo":
+    cont_amarillo = cont_amarillo+1
+
+#USUARIO 5
+
+user5=input("ingrese nombre de usuario:")
+color5=input("ingrese color de su alianza:")
+
+if color5 == "rojo":
+    cont_rojo = cont_rojo+1
+
+if color5 == "azul":
+    cont_azul = cont_azul+1
+
+if color5 == "amarillo":
+    cont_amarillo = cont_amarillo+1
+
+print("la cantidad de personas en la alianza de los rojos son":, cont_rojo)
+print("la cantidad de personas en la alianza de los azules son":, cont_azul)
+print("la cantidad de personas en la alianza de los amarillos son":, cont_amarillo)
+
+
+
