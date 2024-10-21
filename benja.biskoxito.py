16d008d16f85aed622ded0d025f549cfeaa667f1
benjitaa898 <benja.frojas21@gmail.com>
2024-10-21 16:26:11 -0300

    se sube archivo de python

---
+++ benja.biskoxito.py
@@ -0,0 +1,41 @@
+print (bienvenido ala calculadora institutana )
+
+def suma(a, b)
+   return a + b 
+#ungreso de datos
+
+#primera ronda
+
+num1 = float(input("ingrese el primer numero: "))
+print("ingrese el segundo numero: ")
+num2 = float(input(""))
+
+print("")
+
+#segunda ronda 
+x = float("ingrese un numero: ")
+y = float("ingrese otro numero")
+print("")
+
+#tercera ronda 
+n1 = int(input("ingrese nuevamente un numero: "))
+n2 = int(input("ingrese nuevamente otro numero: "))
+
+#cuarta ronda 
+numero1 = 5
+numero2 = 22
+
+print("respuestas")
+print("la suma de ambos numeros es:", suma (num1, num2))
+print("la resta de ambos numeros es:",  resta (x, y))
+print("la multiplicacion de ambos numeros es:", multiplicacion (n1, n2))
+print("la division de ambos numeros ", division (numero1, numero2))
+
+
+
+
+
+
+
+
+
